c28af1d2a988a3cfab412c5ecf7a301e3c9b842c
cipri74 <cipri728@gmail.com>
2022-11-12 19:34:12 +0200

    Refactore and correct getAgeInWeeks formula

--- script.js
+++ script.js
@@ -1,62 +1,77 @@
-function draw_calendar() {
-    var calendar = document.getElementById('calendar')
-    var decades = 8
-    var years = 10
-    var weeks = 52
-    var calendar_age = 1
-    for (let decade=0; decade<decades; decade++){
-        var d = document.createElement('div')
-        d.className = 'decade'
-        for (let year=0; year<years; year++){
-            for(let week=0; week<weeks; week++){
-                var square = document.createElement('div')
-                square.className = 'square'
-                d.appendChild(square)
+const decades = 8
+const years = 10
+const weeks = 52
+
+function drawCalendar() {
+    var calendarTag = document.getElementById('calendar')
+    for (let decade = 0; decade < decades; decade++){
+        let newDecadeTag = document.createElement('div')
+        newDecadeTag.className = 'decade'
+        for (let year = 0; year < years; year++){
+            let ageCalendarIndex = (decade * 10) + year + 1
+            for(let week = 0; week < weeks; week++){
+                let squareTag = document.createElement('div')
+                squareTag.className = 'square'
+                newDecadeTag.appendChild(squareTag)
             }
-            var index = document.createElement('div')
-            index.classList.add('number')
-            if (calendar_age % 5 == 0 || calendar_age == 1){
-                index.innerText = calendar_age
+            let indexTag = document.createElement('div')
+            indexTag.className = 'number'
+            if (ageCalendarIndex % 5 == 0 || ageCalendarIndex == 1){
+                indexTag.innerText = ageCalendarIndex
             }
-            calendar_age ++
-            d.appendChild(index)
+            newDecadeTag.appendChild(indexTag)
         }
-        calendar.appendChild(d)
+        calendarTag.appendChild(newDecadeTag)
     }
 }
 
-function clean_calendar(){
+function cleanCalendar(){
     let squares = document.getElementsByClassName('square')
     for (let i=0;i<squares.length; i++){
         squares[i].classList.remove('square-black')
     } 
 }
 
-function fill_calendar() {
-    clean_calendar()
-    let birth_date = document.getElementById('date-input')
-    if (birth_date.value === ''){
+function getAgeInWeeks(birthDate){
+    var currentDate = new Date()
+    let age = Math.round((currentDate - birthDate) / (1000 * 60 * 60 * 24 * 7))
+    let leapYears = Math.round((currentDate.getFullYear() - birthDate.getFullYear()) / 4) - 1
+    return age - leapYears
+}
+
+function getBirthDate(){
+    var birthDateTag = document.getElementById('date-input')
+    if (birthDateTag.value === ''){
         console.log('Nothing')
-        return 
+        return false
     }
-    let current_date = new Date()
-    birth_date = Date.parse(birth_date.value)
-    let age = Math.floor(Math.floor((current_date.getTime() - birth_date) / (1000 * 60 * 60 * 24)) / 7)
+    birthDate = new Date(birthDateTag.value)
+    let age = getAgeInWeeks(birthDate)
     if (age <= 0){
         console.log('Future Date')
-        return
+        return false
     }
-    console.log(age)
-    let squares = document.getElementsByClassName('square')
-    for (let i=0;i<squares.length; i++){
+    if (age > 52 * decades * years){
+        console.log('Maximum age limit')
+        return false
+    }
+    return age
+}
+
+function fill_calendar() {
+    cleanCalendar()
+    age = getBirthDate()
+    if (!age) return
+    var squareTags = document.getElementsByClassName('square')
+    for (let i = 0; i < squareTags.length; i++){
         age--
-        squares[i].classList.add('square-black');
+        squareTags[i].classList.add('square-black');
         if (age == 0) return
     }
 }
 
 function main() {
-    draw_calendar()
+    drawCalendar()
     document.getElementById('date-button').addEventListener('click', fill_calendar)
 }
 
